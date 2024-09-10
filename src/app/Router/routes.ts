0cbbf9fcce4004368530df94a18961eabe42e538
Lariel <lariel.negreiros@gmail.com>
2024-09-09 23:01:43 -0300

    #1 - padroniza configs base

---
+++ src/app/Router/routes.ts
@@ -0,0 +1,9 @@
+export const routes = {
+	home: '/home',
+	projects: '/projetos',
+	accountability: '/transparência',
+	contribute: '/contribua',
+	contact: '/contato',
+	about: '/sobre',
+	links: '/links'
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	server: {
+		open: true,
+		port: 3001
+	},
+	resolve: {
+		alias: {
+			'@app': path.resolve(__dirname, 'src', 'app'),
+			'@ui': path.resolve(__dirname, 'src', 'ui'),
+		}
+	}
 })
