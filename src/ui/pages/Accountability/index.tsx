abf0d3416c595970ffe2678ac570c8362977b776
Lariel <lariel.negreiros@gmail.com>
2024-10-17 08:13:37 -0300

    #9 - wip relatório transparência

--- src/ui/pages/Accountability/index.tsx
+++ src/ui/pages/Accountability/index.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { ContainerStyled, ContentStyled } from '@ui/styles/GlobalStyles';
 import PageIntro from '@app/components/PageIntro';
 import P from '@app/components/Paragraph';
+import relatorio from '@assets/relatório_financeiro_10-2024.pdf';
 
 export default function Accountability() {
 
@@ -26,7 +27,11 @@ export default function Accountability() {
 			<ContentStyled className='content-responsive'>
 				<PageIntro intro='Aqui você encontra a prestação de contas do clube'/>
 				<P text='Acreditamos que a transparência é fundamental para fortalecer a confiança e o compromisso entre nossos membros, apoiadores e a comunidade.'/>
-				<P text='Nesta página, disponibilizamos todas as informações financeiras do clube de maneira clara e acessível.'/>
+				<P text='Nesta página, disponibilizamos as informações financeiras do clube de maneira clara e acessível.'/>
+				<iframe src={relatorio}
+								width="950"
+								height="800">
+				</iframe>
 			</ContentStyled>
 		</ContainerStyled>
 	);
