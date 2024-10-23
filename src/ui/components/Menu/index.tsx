bbbc7c2dc9d3798edee0618b70b38e8803474f5c
Lariel <lariel.negreiros@gmail.com>
2024-10-23 07:22:53 -0300

    #13 - mantendo opção de menu clicada ativa

--- src/ui/components/Menu/index.tsx
+++ src/ui/components/Menu/index.tsx
@@ -3,17 +3,101 @@ import { routes } from '@app/Router/routes'
 import { Router } from '@app/Router'
 import { MenuContainerStyled, MenuStyled } from './styles';
 
+let isHomeActive = true;
+let isProjectsActive = false;
+let isAccountabilityActive = false;
+let isContributeActive = false;
+let isContactActive = false;
+
 export function Menu() {
+
+	function changeActiveOption(optionSelected: string) {
+		switch (optionSelected) {
+			case routes.home.label:
+				isHomeActive = true;
+				isProjectsActive = false;
+				isAccountabilityActive = false;
+				isContributeActive = false;
+				isContactActive = false;
+				break;
+			case routes.projects.label:
+				isHomeActive = false;
+				isProjectsActive = true;
+				isAccountabilityActive = false;
+				isContributeActive = false;
+				isContactActive = false;
+				break;
+			case routes.accountability.label:
+				isHomeActive = false;
+				isProjectsActive = false;
+				isAccountabilityActive = true;
+				isContributeActive = false;
+				isContactActive = false;
+				break;
+			case routes.contribute.label:
+				isHomeActive = false;
+				isProjectsActive = false;
+				isAccountabilityActive = false;
+				isContributeActive = true;
+				isContactActive = false;
+				break;
+			case routes.contact.label:
+				isHomeActive = false;
+				isProjectsActive = false;
+				isAccountabilityActive = false;
+				isContributeActive = false;
+				isContactActive = true;
+				break;
+			default:
+				isHomeActive = true;
+				isProjectsActive = false;
+				isAccountabilityActive = false;
+				isContributeActive = false;
+				isContactActive = false;
+				break;
+		}
+	}
+
+	function handleclick(node: any) {
+		const optionSelected = node.target.innerText
+		changeActiveOption(optionSelected);
+	}
+
 	return (
 		<>
 			<BrowserRouter>
 				<MenuContainerStyled className='container'>
 					<MenuStyled className='structure-desktop'>
-						<Link className={false ? 'active': 'default'} to={routes.home}>Início</Link>
-						<Link className={false ? 'active': 'default'} to={routes.projects}>Projetos</Link>
-						<Link className={false ? 'active': 'default'} to={routes.accountability}>Transparência</Link>
-						<Link className={false ? 'active': 'default'} to={routes.contribute}>Contribua</Link>
-						<Link className={false ? 'active': 'default'} to={routes.contact}>Contato</Link>
+						<Link
+							className={isHomeActive ? 'active': 'default'}
+							onClick={handleclick}
+							to={routes.home.path}>
+								{routes.home.label}
+						</Link>
+						<Link
+							className={isProjectsActive ? 'active': 'default'}
+							onClick={handleclick}
+							to={routes.projects.path}>
+								{routes.projects.label}
+						</Link>
+						<Link
+							className={isAccountabilityActive ? 'active': 'default'}
+							onClick={handleclick}
+							to={routes.accountability.path}>
+								{routes.accountability.label}
+						</Link>
+						<Link
+							className={isContributeActive ? 'active': 'default'}
+							onClick={handleclick}
+							to={routes.contribute.path}>
+								{routes.contribute.label}
+						</Link>
+						<Link
+							className={isContactActive ? 'active': 'default'}
+							onClick={handleclick}
+							to={routes.contact.path}>
+								{routes.contact.label}
+						</Link>
 					</MenuStyled>
 				</MenuContainerStyled>
 				<Router />
