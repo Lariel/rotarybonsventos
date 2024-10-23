bdad7b4327e172477a7bc680fc67cdedb10aa189
Lariel <lariel.negreiros@gmail.com>
2024-10-23 07:53:32 -0300

    #13 - modelagem do objeto com as páginas

---
+++ src/app/model/Pages.ts
@@ -0,0 +1,42 @@
+export const Pages  = {
+	home: {
+		path: '/',
+		label: 'Início',
+		description: 'Bem vindo'
+	},
+	projects: {
+		path: '/projetos',
+		label: 'Projetos',
+		description: 'Nossos projetos'
+	},
+	projectDetails: {
+		path: '/projetos/:id',
+		label: '',
+		description: ''
+	},
+	accountability: {
+		path: '/transparência',
+		label: 'Transparência',
+		description: 'Prestação de contas'
+	},
+	contribute: {
+		path: '/contribua',
+		label: 'Contribua',
+		description: 'Contribua'
+	},
+	contact: {
+		path: '/contato',
+		label: 'Contato',
+		description: 'Nossos contatos'
+	},
+	about: {
+		path: '/sobre',
+		label: '',
+		description: ''
+	},
+	links: {
+		path: '/links',
+		label: '',
+		description: ''
+	}
+};
