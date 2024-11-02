23667f7b02e30b90d7c00eb74e62326b44e66c4b
Lariel <lariel.negreiros@gmail.com>
2024-11-02 18:58:37 -0300

    #7 - ordenação de projetos por id

--- src/app/services/ProjectService.ts
+++ src/app/services/ProjectService.ts
@@ -2,10 +2,10 @@ import { projects } from '@app/model/Projects';
 import { Project } from '@app/types/Project';
 
 export function getProjects(): Project[] {
-	return projects;
+	return projects.sort((a, b) => a.id - b.id);
 }
 
 export function getHighlightProjects(): Project[] {
-	return projects.filter(project => project.highlight);
+	return projects.filter(project => project.highlight).sort((a, b) => a.id - b.id);
 }
 
