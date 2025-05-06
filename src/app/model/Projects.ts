12005c547a4bc72a53e1698f4f9ad73a7f96987d
Lariel <lariel.negreiros@gmail.com>
2025-05-06 10:49:19 -0300

    #17 - Atualiza descrição projetos

--- src/app/model/Projects.ts
+++ src/app/model/Projects.ts
@@ -30,10 +30,10 @@ export const projects: Project[] = [
 		id: 2,
 		image: `${education}`,
 		title: 'Dia da Saúde',
-		summary: 'Breve descrição do segundo projeto a ser mostrado na página',
+		summary: 'O Dia da Saúde ofereceu serviços gratuitos de saúde à comunidade, promovendo prevenção e bem-estar',
 		info: `O Projeto Dia da Saúde foi uma ação comunitária promovida pelo Rotary Club dos Bons Ventos em parceria com a Unicnec e a Prefeitura Municipal de Osório. Seu objetivo era oferecer, de forma gratuita, serviços e informações essenciais sobre cuidados primários de saúde à população.
 					Durante o evento, a comunidade teve acesso a atendimentos em diversas áreas da saúde, como, testes de acuidade visual e motricidade ocular, atendimento odontológico, psicologia, fisioterapia, farmácia, nutrição e enfermagem.
-					A iniciativa reforça nosso compromisso com a prevenção, a promoção da saúde e o cuidado com o bem-estar coletivo. Para nós, rotarianos, servir é transformar — e cuidar da saúde é um dos caminhos mais importantes para fortalecer comunidades e garantir qualidade de vida
+					A iniciativa reforça nosso compromisso com a prevenção, a promoção da saúde e o cuidado com o bem-estar coletivo. Para nós, rotarianos, servir é transformar — e cuidar da saúde é um dos caminhos mais importantes para fortalecer comunidades e garantir qualidade de vida.
 				`,
 		highlight: false,
 		tags: [tag3, tag4]
@@ -42,7 +42,7 @@ export const projects: Project[] = [
 		id: 3,
 		image: `${health}`,
 		title: 'Arrecadação de Alimentos',
-		summary: 'Breve descrição do terceiro projeto a ser mostrado na página',
+		summary: 'Arrecadação de alimentos na Semana Farroupilha para apoiar entidades assistenciais de Osório',
 		info: `Durante dois anos consecutivos, o Rotary Club dos Bons Ventos promoveu uma ação solidária durante a Semana Farroupilha, arrecadando alimentos para apoiar entidades que prestam assistência à comunidade. As doações foram entregues à Pastoral da Criança, ao Centro Espírita e ao Lar dos Idosos de Osório, fortalecendo laços e levando acolhimento a quem mais precisa.`,
 		highlight: true,
 		tags: [tag3, tag4, tag5]
@@ -51,7 +51,7 @@ export const projects: Project[] = [
 		id: 4,
 		image: `${peace}`,
 		title: 'Campanha do agasalho',
-		summary: 'Breve descrição do quarto projeto a ser mostrado na página',
+		summary: 'Campanha do agasalho realizada em parceria com o Rotaract, destinada às vítimas de um ciclone na região',
 		info: `Em parceria com o Rotaract Club de Osório, promovemos uma campanha do agasalho com foco em organização e impacto. Todas as doações foram cuidadosamente triadas, classificadas e destinadas às vítimas de um ciclone extratropical que atingiu a região. Um gesto de empatia que aqueceu tanto os corpos quanto os corações.`,
 		highlight: true,
 		tags: [tag3, tag4, tag5]
@@ -60,7 +60,7 @@ export const projects: Project[] = [
 		id: 5,
 		image: `${nature}`,
 		title: 'Blitz da Pólio',
-		summary: 'Breve descrição do quinto projeto a ser mostrado na página',
+		summary: 'Campanha e distribuição de panfletos para conscientizar sobre a importância da vacinação infantil na erradicação da pólio',
 		info: `Com o compromisso de contribuir para um mundo livre da pólio, realizamos uma blitz informativa com distribuição de panfletos, reforçando a importância da vacinação infantil. A ação teve como foco conscientizar pais e responsáveis sobre a necessidade da imunização e o papel vital que ela desempenha na erradicação da doença.`,
 		highlight: true,
 		tags: [tag2, tag3, tag4, tag5]
@@ -69,7 +69,7 @@ export const projects: Project[] = [
 		id: 6,
 		image: `${projVoltaAulas}`,
 		title: 'SOS Chuvas – Apoio às Vítimas das Enchentes no RS',
-		summary: 'Breve descrição do sexto projeto a ser mostrado na página',
+		summary: 'Diante das enchentes no RS, o Rotary Bons Ventos mobilizou doações de todo o Brasil, enviando mais de 5 caminhões com itens essenciais às áreas afetadas',
 		info: `Diante das fortes enchentes que atingiram o Rio Grande do Sul, o Rotary Club de Osório Bons Ventos se mobilizou intensamente em uma corrente de solidariedade. Arrecadamos, separamos e distribuímos doações vindas de diferentes partes do Brasil, incluindo São Paulo e Santa Catarina. Foram mais de 5 caminhões carregados com mantimentos, água potável, roupas, colchões e kits de higiene, enviados diretamente para as áreas afetadas. Um esforço coletivo movido pelo nosso compromisso de estar presente onde a ajuda é mais urgente.`,
 		highlight: false,
 		tags: [tag3, tag4]
@@ -78,7 +78,7 @@ export const projects: Project[] = [
 		id: 7,
 		image: `${education}`,
 		title: 'Galeto do Bons Ventos',
-		summary: 'Breve descrição do sétimo projeto a ser mostrado na página',
+		summary: 'O Galeto do Bons Ventos é uma ação solidária que arrecada fundos para projetos sociais. Na última edição, foram vendidos mais de 350 ingressos, gerando mais de R$10 mil em recursos',
 		info: `O Galeto do Bons Ventos é uma ação financeira do nosso clube, que une sabor e solidariedade. Através da venda de galetos assados, arrecadamos fundos para manter e viabilizar nossos projetos sociais ao longo do ano rotário. Cada refeição vendida representa um gesto de apoio à nossa missão de servir e transformar realidades na comunidade.
 Na última edição vendemos mais de 350 ingressos que davam direito a 4 pedaços de frango, maionese, arroz e salada. Conseguimos arrecadar mais de 10 mil reais e assim manter nossas ações e projetos ativos.
 `,
@@ -89,7 +89,7 @@ Na última edição vendemos mais de 350 ingressos que davam direito a 4 pedaço
 		id: 8,
 		image: `${health}`,
 		title: 'Dia das Crianças',
-		summary: 'Breve descrição do oitavo projeto a ser mostrado na página',
+		summary: 'No Dia das Crianças, levamos brinquedos e lanches a um bairro carente, promovendo um dia de alegria e inclusão para as crianças',
 		info: `Em comemoração ao Dia das Crianças, promovemos uma ação especial em um bairro que raramente recebe iniciativas voltadas ao público infantil. Alugamos brinquedos infláveis e distribuímos cachorro-quente, refrigerante e pipoca, criando um momento de alegria, cuidado e diversão para as crianças da comunidade. Um gesto simples, mas cheio de significado, que reforça o nosso compromisso com o bem-estar e o desenvolvimento infantil.`,
 		highlight: false,
 		tags: []
@@ -98,7 +98,7 @@ Na última edição vendemos mais de 350 ingressos que davam direito a 4 pedaço
 		id: 9,
 		image: `${peace}`,
 		title: 'Conexão Natal do Bem',
-		summary: 'Breve descrição do nono projeto a ser mostrado na página',
+		summary: 'O Conexão Natal do Bem levou alegria e inclusão às crianças com festa, presentes e praça revitalizada, em parceria com o Rotaract de Osório',
 		info: `Conexão Natal do Bem é um projeto realizado em parceria com o Rotaract Club de Osório, com o objetivo de levar esperança e alegria no fim de ano. Reformamos a praça Pedro Farias e organizamos um dia mágico com brinquedos infláveis, cachorro-quente, pipoca, refrigerante e entrega de presentes. A ação promove um Natal mais digno para as crianças do bairro, proporcionando não apenas diversão, mas também afeto e inclusão.`,
 		highlight: false,
 		tags: [tag4, tag5, tag1]
